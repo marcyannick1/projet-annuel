395b82ca397399b610f2668684d4da209ab4e97e
rufus232 <mouakassarufus@gmail.com>
2024-08-22 13:47:10 +0200

    "maj des pages login/register"

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,36 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
-import { Alert } from 'antd';
+import LoginJSX from './components/login/LoginJSX'
+import SignJSX from './components/sign/SignJSX'
+import UsersJSX from './components/users/UsersJSX'
+import BackgroundJSX from './components/background/BackgroundJSX'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Alert message="Success Text" type="success" />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+    <div className="content">
+      <Routes>
+        <Route path='/' element={<LoginJSX />} />
+        <Route path='/BackgroundJSX' element={<BackgroundJSX />} />
+        <Route path='/SignJSX' element={<SignJSX />} />
+        <Route path='/UsersJSX' element={<UsersJSX />} />
+
+
+
+      </Routes>
+    </div>
+  </Router>
   )
 }
 
