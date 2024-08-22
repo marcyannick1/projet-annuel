4caa095f3b4f10f486c080caf8da977e12de512b
Marc Yannick <yannickcoulibaly220@gmail.com>
2024-08-22 17:26:20 +0200

    login/register

--- back/index.js
+++ back/index.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const bodyParser = require("express");
 
 dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+    cors({
+        origin: "*",
+        method: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+    })
+);
+
+app.use("/", require("./routes/auth"))
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Serveur en écoute sur http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
+});
