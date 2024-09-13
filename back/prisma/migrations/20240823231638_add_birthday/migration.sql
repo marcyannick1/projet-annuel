472ccb2d1180836932dcc82d5d4e73fbe267b9f8
Marc Yannick <yannickcoulibaly220@gmail.com>
2024-09-13 14:57:49 +0200

    merge front/subscription

--- back/prisma/migrations/20240823231638_add_birthday/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `birthday` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `birthday` DATETIME(3) NOT NULL;
